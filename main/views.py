04445fda8459c637de5ecf0d57db60de35b99d30
yutomurao <voxyyuto@gmail.com>
2025-09-07 16:04:12 +0900

    9/7終了(ビューーーー)

--- main/views.py
+++ main/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("おはよう！藤原先生！")
+
+def hobby(request):
+    return HttpResponse("私の趣味は野球観戦です。")
+
+def greet(request, name):
+    message = "こんにちは ", name, " さん！！"
+    return HttpResponse(message)

b3fcb857d3ef6d5d99dc0c3ffcbbf3825ce20c8e
yutomurao <voxyyuto@gmail.com>
2025-09-20 18:31:21 +0900

    もでるを編集したよ（管理者サイトに侵入した）

--- main/views.py
+++ main/views.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
 
+from .models import Question
 
 def index(request):
-    question_list = [
-        "あなたは子どもですか？",
-        "あなたは大人ですか？",
-        "あなたは老人ですか",
-        "あなたは藤原先生ですか？",
-    ]
+    all_question = Question.objects.all()
     context = {
-        "question_list": question_list,
-        "is polled": True,
-        "polled_msg": "投票していただき、本当にありがとうございました。",
-        "not_polled_msg": "投票早くしろやこら！！早く、早く、早く、早く、早く、早く、早く、早く",
-        "user_name": "たろー",
+        "all_question": all_question
     }
+
     return render(request, "main/index.html", context)
